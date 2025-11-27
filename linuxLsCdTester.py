3b72a4cc65ef2123f71eae9c2771d1a15c475c64
JamesCorradinoGit <corradinojames34@gmail.com>
2025-11-26 21:03:00 -0600

    addchild method lscd

--- linuxLsCdTester.py
+++ linuxLsCdTester.py
@@ -4,15 +4,18 @@ import random
 currentLocal = 0 #0 is root terminal, increased num = deeper in "files"
 numDirects = random.randint(3, 6)
 
-class Node:
-    def __init__(self, data):
-        self.data = data
+class Node: #USE SELF ON ALL INSTANCE METHODS
+    def __init__(self, nodeName:str):
+        self.nodeName = nodeName
         self.children = []
     
-    
+    def addChild(self, name:str):
+        self.children.append(name)
     
     def __str__(self):
-        return str(self.data)
+        return ("Node name: " + self.nodeName + ", Node contents: " + str(self.children))
 
-test = Node([1,"hi"])
+test = Node("Documents")
+test.addChild("goon.txt")
+test.addChild("brainrot.txt")
 print(test)
