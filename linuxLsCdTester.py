9bb3c034daf812eb9f4d6b325fb48a2a706ecc96
JamesCorradinoGit <corradinojames34@gmail.com>
2025-11-30 21:35:54 -0600

    semi-wroking cd and ls functions

--- linuxLsCdTester.py
+++ linuxLsCdTester.py
@@ -1,14 +1,7 @@
 import tkinter as tk
 import random
 
-innitDirects = {}
-numDirectsToInnit = random.randint(2, 3)
-
-docDirectNameList = ["a","b","c","d","e","f","g"] 
-imagDirectNameList = []
-vidDirectNameList = []
-# ^THESE ARE NOT FILE NAMES!! THESE ARE FOLDER-ESQ NAMES
-
+#region Nodes
 class Node: #USE SELF ON ALL INSTANCE METHODS
     def __init__(self, nodeName:str):
         self.nodeName = nodeName
@@ -21,12 +14,14 @@ class Node: #USE SELF ON ALL INSTANCE METHODS
     def addChildren(self, dat):
         self.children.extend(dat)
 
-    def checkForNodeChild(self):
+    def checkForChild(self, targetChildName):
         for i in range(0, len(self.children)):
-            if isinstance(self.children[i], Node):
-                print(self.children[i].nodeName)
+            if self.children[i].nodeName == targetChildName:
+                return True
+            else:
+                return False
                 
-    def findChild(self, targetChildName):
+    def getChild(self, targetChildName): #optimize with index command?
         for i in range(0, len(self.children)):
             if self.children[i].nodeName == targetChildName:
                 return self.children[i]
@@ -39,15 +34,53 @@ class Node: #USE SELF ON ALL INSTANCE METHODS
 def populateNode(nodeToAddD:Node, directList:list, numNodesToAdd):
     tempAddList = []
     for i in range(0, numNodesToAdd):
-        tempAddList.append(directList[i])
+        tempAddList.append(Node(directList[i]))
     nodeToAddD.addChildren(tempAddList)
 
 def createRootNodes():
     rootNode = Node("root")
     tempNodes = [Node("Documents"), Node("Pictures"), Node("Videos")]
     rootNode.addChildren(tempNodes)
-    return rootNode
 
+    docDirectNameList = ["Homework","Worksheets","VisualStudio","GoogleDocs","MicrosoftWord","CipherKit"]
+    imagDirectNameList = ["GraduationPhotos", "Screenshots", "MobilePictures", "SlideshowAssets", "StockImages", "GIFs"] 
+    vidDirectNameList = ["n", "o", "p", "q", "r", "s"]
+    # ^THESE ARE NOT FILE NAMES!! THESE ARE FOLDER-ESQ NAMES
+
+    populateNode(rootNode.getChild("Documents"), docDirectNameList, random.randint(2,6))
+    populateNode(rootNode.getChild("Pictures"), imagDirectNameList, random.randint(2,6))
+    populateNode(rootNode.getChild("Videos"), vidDirectNameList, random.randint(2,6))
+    return rootNode
+#endregion
+
+termName = "root"
 instRootNodes = createRootNodes()
-populateNode(instRootNodes.findChild("Documents"), docDirectNameList, 2)
-print(instRootNodes.findChild("Documents"))
+currentNode = instRootNodes
+
+def lsSim(nodeToList:Node):
+    print(nodeToList.children)
+
+def cdSim(rootNode:Node, input:str):
+    global termName
+    global currentNode
+    tempTermName = ""
+    if input == "..":
+        nodeStrToRemove = termName[termName.rfind("/"):]
+        tempTermName = termName.removesuffix(nodeStrToRemove)
+        currentStrToRemove = termName[termName.find("/")+1:termName.rfind("/"):]
+        currentNode = rootNode.getChild(currentStrToRemove)
+        
+        termName = tempTermName
+    elif currentNode.checkForChild(input):
+        tempTermName = termName + "/"+input
+        currentNode = currentNode.getChild(input)
+        
+        termName = tempTermName
+    else:
+        print("Please route to a valid directory.")
+
+cdSim(instRootNodes, "Documents")
+lsSim(currentNode)
+cdSim(instRootNodes, "Homework")
+cdSim(instRootNodes, "..")
+print(termName)
