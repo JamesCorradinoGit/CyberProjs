252ac224a2fb7802da437158ef31f1e9d17979ba
JamesCorradinoGit <corradinojames34@gmail.com>
2025-12-01 07:48:12 -0600

    working hash

--- hashEncoder.py
+++ hashEncoder.py
@@ -1,8 +1,15 @@
 import hashlib
 import tkinter as tk
 
-for i in range(0,5):
-    print("hi")
+def encodeString(str):
+    md5 = hashlib.md5()
+    str = str.encode()
 
+    md5.update(str)
+    strToReturn = md5.hexdigest()
+
+    return(strToReturn)
 root = tk.Tk()
+print(encodeString("hi guys"))
+
 root.mainloop()
