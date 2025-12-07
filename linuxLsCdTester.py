8f89b154df3936f1fd3b3b3d8ee40074339f3104
JamesCorradinoGit <corradinojames34@gmail.com>
2025-12-06 21:03:17 -0600

    working v1

--- linuxLsCdTester.py
+++ linuxLsCdTester.py
@@ -1,29 +1,55 @@
 import tkinter as tk
 import random
+import time
 
 termName = "root"
+score = 0
+currentTargetStr = ""
 
 #region tk
 rootTk = tk.Tk()
-rootTk.config(background="grey8") #TODO finish ui
+rootTk.config(background="grey8") 
 rootTk.geometry("400x200")
+rootTk.resizable(False, False)
 
+def outputText(inputText, colStyle):
+    outputField.delete("1.0", tk.END)
+    outputField.insert(tk.END, inputText+"\n", colStyle)
 def submitPrompt(event):
     inputtedStr = commandPrompt.get()
-    if inputtedStr.startswith("cd"):
+    if inputtedStr.startswith("@"):
+        adminCmds(inputtedStr)
+    elif inputtedStr.startswith("cd"):
         improvImputtedStr = inputtedStr[inputtedStr.find("cd")+3:]
         cdSim(instRootNodes, improvImputtedStr)
     elif inputtedStr.startswith("ls"):
         lsSim(currentNode)
-    event.widget.delete(0, tk.END) #TODO check for cd or ls and the input
+    elif inputtedStr.lower() == "check":
+        checkTarget(currentTargetStr)
+    else:
+        outputText("ERROR: Not a valid command.", "ErrorText")
+    event.widget.delete(0, tk.END)
 
-directNameLabel = tk.Label(rootTk, text=termName, font=("Courier New", 10, "bold"), fg="white", bg="grey8").place(x=15, y=10)
+directNameLabel = tk.Label(rootTk, text=termName, font=("Courier New", 10, "bold"), fg="white", bg="grey8")
+directNameLabel.place(x=15, y=10)
+def updateMainLabel():
+    directNameLabel.config(text=termName)
+
+tk.Label(rootTk, text="Output:", font=("Courier New", 10, "bold"), fg="white", bg="grey8").place(x=15, y=60)
+outputField = tk.Text(width=45, height= 6, font=("Courier New", 10, "bold"), fg="white", bg="grey8", highlightthickness=1, highlightcolor="white", highlightbackground="white", insertbackground="white")
+outputField.bind("<Key>", lambda breakieBreaker: "break")
+
+outputField.tag_config("ErrorText", foreground="red")
+outputField.tag_config("CheckText", foreground="DeepSkyBlue2")
+
+outputField.place(x=15, y=85)
+
 commandPrompt = tk.Entry(rootTk, width=45, bg="grey8", font=("Courier New", 10, "bold"), fg="white", highlightthickness=1, highlightcolor="white", highlightbackground="white", insertbackground="white")
 commandPrompt.bind("<Return>", submitPrompt)
 commandPrompt.place(x=15, y=33)
 #endregion
 
-#region Functionality
+#region cdlsFunctionality
 #region Nodes
 class Node: #USE SELF ON ALL INSTANCE METHODS
     def __init__(self, nodeName:str):
@@ -51,7 +77,7 @@ class Node: #USE SELF ON ALL INSTANCE METHODS
     def __str__(self):
         return ("Node name: " + self.nodeName + ", Node contents: " + str(self.children))
     def __repr__(self):
-        return (self.nodeName+"Node")
+        return (self.nodeName)
 
 def populateNode(nodeToAddD:Node, directList:list, numNodesToAdd):
     tempAddList = []
@@ -65,8 +91,8 @@ def createRootNodes():
     rootNode.addChildren(tempNodes)
 
     docDirectNameList = ["Homework","Worksheets","VisualStudio","GoogleDocs","MicrosoftWord","CipherKit"]
-    imagDirectNameList = ["GraduationPhotos", "Screenshots", "MobilePictures", "SlideshowAssets", "StockImages", "GIFs"] 
-    vidDirectNameList = ["n", "o", "p", "q", "r", "s"]
+    imagDirectNameList = ["GradPhotos", "Screenshots", "MobilePictures", "SlideshowAssets", "StockImages", "GIFs"] 
+    vidDirectNameList = ["Captures", "NVIDIA", "gallery", "Games", "Replays", "Saves"]
     # ^THESE ARE NOT FILE NAMES!! THESE ARE FOLDER-ESQ NAMES
 
     populateNode(rootNode.getChild("Documents"), docDirectNameList, random.randint(2,6))
@@ -85,36 +111,86 @@ def createTarget(rootNode:Node):
     randChildDirect:Node = randParentDirect.children[random.randint(0, len(randParentDirect.children)-1)]
     targetToReturn += "/"+str(randChildDirect.nodeName)
     return(targetToReturn)
-
 def checkTarget(targetStr:str):
+    global score
     if termName == targetStr:
+        score += 1
+        outputText("Correct!", "CheckText")
+        updateScore()
+        updateTarget()
         return True
     else:
+        outputText("Wrong! Try again!", "CheckText")
         return False
 
 def lsSim(nodeToList:Node):
     if type(nodeToList) is Node:
-        print(nodeToList.children)
-#TODO fix cd to root
+        outputText(str(nodeToList.children)+"\n", "")
 def cdSim(rootNode:Node, input:str): #INTENDED TO ONLY USE TWO DIRECTORIES (eg. root/Documents/Homework is the furthest you can go) 
     global termName
     global currentNode
-    tempTermName = ""
-    if input == "..":
-        nodeStrToRemove = termName[termName.rfind("/"):]
-        tempTermName = termName.removesuffix(nodeStrToRemove)
-        currentStrToRemove = termName[termName.find("/")+1:termName.rfind("/"):]
-        currentNode = rootNode.getChild(currentStrToRemove)
-        
-        termName = tempTermName
-    elif currentNode.checkForChild(input):
-        tempTermName = termName + "/"+input
-        currentNode = currentNode.getChild(input)
-        
-        termName = tempTermName
+    if currentNode == instRootNodes and input == "..":
+        outputText("ERROR: You cannot go past the root.\n", "ErrorText")
     else:
-        print("Please route to a valid directory.")
+        tempTermName = ""
+        if input == "..":
+            nodeStrToRemove = termName[termName.rfind("/"):]
+            tempTermName = termName.removesuffix(nodeStrToRemove)
+            currentStrToRemove = termName[termName.find("/")+1:termName.rfind("/"):]
+            if tempTermName == "root":
+                currentNode = instRootNodes
+            else:
+                currentNode = rootNode.getChild(currentStrToRemove)
+            termName = tempTermName
+            updateMainLabel()
+        elif currentNode.checkForChild(input):
+            tempTermName = termName + "/"+input
+            currentNode = currentNode.getChild(input)
+            
+            termName = tempTermName
+            updateMainLabel()
+        else:
+            outputText("ERROR: Please route to a valid directory.\n", "ErrorText")
+
 #endregion
 
-createTarget(instRootNodes)
+#region gameFunctionality&tk
+targetLabel = tk.Label(rootTk, text="TARGET DIRECTORY", font=("Courier New", 10, "bold"), fg="DeepSkyBlue2", bg="grey8")
+scoreLabel = tk.Label(rootTk, text="Score: "+str(score), font=("Courier New", 10, "bold"), fg="DeepSkyBlue2", bg="grey8")
+
+def gameReset():
+    global currentNode, score, instRootNodes, termName
+    termName = "root"
+    targetLabel.config(text="TARGET DIRECTORY")
+    currentNode = instRootNodes
+    updateMainLabel()
+    instRootNodes = createRootNodes()
+    targetLabel.place_forget()
+    scoreLabel.place_forget()
+    createTarget(instRootNodes)
+def updateTarget():
+    global instRootNodes, currentTargetStr
+    trgt = createTarget(instRootNodes)
+    targetLabel.config(text=trgt)
+    currentTargetStr = trgt
+def startGame():
+    targetLabel.place(x=75, y=60)
+    scoreLabel.place(x=310, y=60)
+    updateTarget()
+def resetScore():
+    global score
+    score = 0
+def updateScore():
+    scoreLabel.config(text="Score: "+str(score))
+
+def adminCmds(input:str):
+    improvImputtedStr = input[input.find("@")+1:].replace(" ","")
+    if improvImputtedStr == "reset":
+        gameReset()
+    elif improvImputtedStr == "start":
+        startGame()
+    elif improvImputtedStr == "resetscore":
+        resetScore()
+#endregion
+
 rootTk.mainloop()
